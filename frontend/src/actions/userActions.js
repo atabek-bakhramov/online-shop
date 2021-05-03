ba4108ebe89bdf8d421b9e47c0bf0d89fd415f74
atabek-bakh <coderonline@icloud.com>
2021-05-03 17:34:45 +0200

    Checkout Process is set

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -13,31 +13,29 @@ import {
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
+  USER_DETAILS_RESET,
 } from "../constants/userConstants";
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
       type: USER_LOGIN_REQUEST,
     });
-
     const config = {
       headers: {
         "Content-Type": "application/json",
       },
     };
-
     const { data } = await axios.post(
       "/api/users/login",
       { email, password },
       config
     );
-
     dispatch({
       type: USER_LOGIN_SUCCESS,
       payload: data,
     });
-
     localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
@@ -49,10 +47,11 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
-
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -60,29 +59,24 @@ export const register = (name, email, password) => async (dispatch) => {
     dispatch({
       type: USER_REGISTER_REQUEST,
     });
-
     const config = {
       headers: {
         "Content-Type": "application/json",
       },
     };
-
     const { data } = await axios.post(
       "/api/users",
       { name, email, password },
       config
     );
-
     dispatch({
       type: USER_REGISTER_SUCCESS,
       payload: data,
     });
-
     dispatch({
       type: USER_LOGIN_SUCCESS,
       payload: data,
     });
-
     localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
@@ -94,26 +88,21 @@ export const register = (name, email, password) => async (dispatch) => {
     });
   }
 };
-
 export const getUserDetails = (id) => async (dispatch, getState) => {
   try {
     dispatch({
       type: USER_DETAILS_REQUEST,
     });
-
     const {
       userLogin: { userInfo },
     } = getState();
-
     const config = {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${userInfo.token}`,
       },
     };
-
     const { data } = await axios.get(`/api/users/${id}`, config);
-
     dispatch({
       type: USER_DETAILS_SUCCESS,
       payload: data,
@@ -128,26 +117,21 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
     });
   }
 };
-
 export const updateUserProfile = (user) => async (dispatch, getState) => {
   try {
     dispatch({
       type: USER_UPDATE_PROFILE_REQUEST,
     });
-
     const {
       userLogin: { userInfo },
     } = getState();
-
     const config = {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${userInfo.token}`,
       },
     };
-
     const { data } = await axios.put(`/api/users/profile`, user, config);
-
     dispatch({
       type: USER_UPDATE_PROFILE_SUCCESS,
       payload: data,
